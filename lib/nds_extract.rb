d771157c3fec96e1d17e56c921b1f0fd86a49810
Lugh Wilson <lughjw@gmail.com>
2020-01-01 16:50:38 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,7 +4,7 @@ require 'directors_database'
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  pp(director_data)
+  director_data[:worldwide_grosses]
   
 end
 
@@ -15,6 +15,7 @@ end
 def directors_totals(nds)
   result = {}
   
+  for 
   
   
   result
